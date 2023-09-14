b2752c7986e2d84da297a164642e0eec3c9d094d
SheltaV <bluegiraffezaoz@gmail.com>
2023-09-14 13:12:07 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,23 +1,23 @@
 import { useEffect } from 'react';
 import { Overlay, ModalWindow } from './Modal.styled';
 
-export const Modal = ({imageValue, backdropClick, toggleModal}) => {
+export const Modal = ({imageValue, backdropClick}) => {
 
   useEffect(() => {
+  const handleKeydown = e => {
+    if (e.code === 'Escape') {
+        backdropClick()
+      }
+    }
+
     window.addEventListener('keydown', handleKeydown)
     
     return () => {
     window.removeEventListener('keydown', handleKeydown)
     }
-}, [])
+}, [backdropClick])
   
-const handleKeydown = e => {
-    if (e.code === 'Escape') {
-        backdropClick()
-      }
-  }
-  
-  
+
   return (
     <Overlay onClick={() => backdropClick()}>
     <ModalWindow>
